b5b9b7a358de3b43d2c0fae1e10cf1b1ced78c96
Busra Uludag <busrauludag13@gmail.com>
2020-09-03 22:22:05 +0300

    added bottom nav navigator

--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -33,6 +33,17 @@ const CategoryGridTile = props => {
 }
 
 const styles = StyleSheet.create({
+  gridItem: {
+    flex: 1,
+    margin: 15,
+    height: 150,
+    borderRadius: 10,
+    overflow: 
+      Platform.OS === 'android' && Platform.Version >= 21 
+      ? 'hidden' 
+       : 'visible',
+    elevation: 5
+  },
   container: {
     flex: 1,
     borderRadius: 10,
@@ -40,17 +51,9 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.2,
     shadowOffset: { width: 0, height: 2 },
     shadowRadius: 10,
-    elevation: 3,
     padding: 15,
     justifyContent: 'flex-end',
     alignItems: 'flex-end'
-  },
-  gridItem: {
-    flex: 1,
-    margin: 15,
-    height: 150,
-    borderRadius: 10,
-    overflow: 'hidden'
   },
   title: {
     fontFamily: 'open-sans-bold',
