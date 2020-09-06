3f737d2078f045f3f7c4191fc0ffe7e08f583036
Busra Uludag <busrauludag13@gmail.com>
2020-09-06 22:50:46 +0300

    added navigator some style add text wrapper component

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Platform } from 'react-native';
+import { Text, Platform } from 'react-native';
 
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
@@ -20,6 +20,12 @@ const defaultStackNavOptions = {
   navigationOptions: {
     headerStyle: {
       backgroundColor: Platform.OS === 'android' ? Colors.primaryColor : Colors.white
+    },
+    headerTitleStyle: {
+      fontFamily: 'open-sans-bold'
+    },
+    headerBackTitleStyle: {
+      fontFamily: 'open-sans'
     },
     headerTintColor: Platform.OS === 'android' ? Colors.white : Colors.primaryColor
   }
@@ -65,7 +71,9 @@ const tabScreenConfig = {
           <Ionicons name='ios-restaurant' size={25} color={tabInfo.tintColor} />
         )
       },
-      tabBarColor: Colors.primaryColor
+      tabBarColor: Colors.primaryColor,
+      tabBarLabel: Platform.OS === 'android' ? <Text style={{ fontFamily: 'open-sans-bold' }}>Meals</Text> : 'Meals'
+
     }
   },
   Favorites: {
@@ -77,7 +85,8 @@ const tabScreenConfig = {
           <Ionicons name='ios-star' size={25} color={tabInfo.tintColor} />
         )
       },
-      tabBarColor: Colors.accentColor
+      tabBarColor: Colors.accentColor,
+      tabBarLabel: Platform.OS === 'android' ? <Text style={{ fontFamily: 'open-sans-bold' }}>Favorites</Text> : 'Favorites'
     }
   }
 };
@@ -100,6 +109,9 @@ const MealsFavTabNavigator =
       tabScreenConfig,
       {
         tabBarOptions: {
+          labelStyle: {
+            fontFamily: 'open-sans-bold'
+          },
           activeTintColor: Colors.accentColor
         }
       }
